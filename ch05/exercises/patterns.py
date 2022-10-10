49d4630ecedec3836b988bc7bcf5988b3944f968
Lillian Thorne <112438914+lthorne3@users.noreply.github.com>
2022-10-10 21:46:58 +0000

    patterns

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,7 +1,18 @@
 def star_pyramid():
-  rows= int(input("How many rows?"))
-  for i in range (0, rows):
-    for j in range(0, i+1):
-      print ("*\n")
-  star_pyramid()    
-  
+    rows = int(input("How many rows?"))
+    for i in range(0, rows):
+        for j in range(0, i + 1):
+            print('*', end='')
+        print("\n")
+
+
+def rstar_pyramid():
+    rows = int(input("How many rows?"))
+    while rows > 0:
+        print('*'*rows, end='')
+        rows = rows - 1
+        print("\n")
+
+
+star_pyramid()
+rstar_pyramid()
