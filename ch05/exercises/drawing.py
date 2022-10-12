df1da8c68085624a56f8d1a37e401022cd23413d
Lillian Thorne <112438914+lthorne3@users.noreply.github.com>
2022-10-12 20:57:24 +0000

    drawing

--- ch05/exercises/drawing.py
+++ ch05/exercises/drawing.py
@@ -1,14 +1,16 @@
 import turtle
 
+window=turtle.Screen()
 t= turtle.Turtle()
 t.shape("turtle")
 t.color("green")
-window=turtle.Screen()
+num_sides= int(input("How many sides? "))
+side_length= int(input("How long is each side? "))
 
 def drawEqShape():
   t
-  num_sides= int(input("How many sides? "))
-  side_length= int(input("How long is each side? "))
+  num_sides
+  side_length
   angle= 360/num_sides
   for i in [angle]*num_sides:
     t.forward(side_length)
