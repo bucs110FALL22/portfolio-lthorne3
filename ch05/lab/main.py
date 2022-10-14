a6f2c73765a7012f5e9b54865ce48bf449e148af
Lillian Thorne <112438914+lthorne3@users.noreply.github.com>
2022-10-14 23:04:25 +0000

    ch5 lab

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,19 +1,45 @@
-def foo(n):
-  count = 0
-  nums=[n]
-  for i in [nums]:
-    if n%2==0:
-      n=n/2
-      count+=1
-    else:
-      n= (3*n)+1
-      count+=1
-    
-    if n==1:
-      break
-    print(n)
-    print("count =", count)
-    return n
-    
-foo(101)
+import pygame
 
+coords = []
+n=None
+count=None
+max_so_far=None
+upper_limit=200
+
+
+def foo(n):
+    count = 0
+    max_so_far = count 
+    while n != 1:
+        coords.append([n, count])
+        print(n, end=', ')
+        if n % 2 == 0:
+            n = n // 2
+            count += 1
+        else:
+            n = n * 3 + 1
+            count += 1
+
+      
+    print(n, end='. ')
+    print("count =", count)
+    if count > max_so_far:
+        max_so_far = count
+    for i in [coords]:
+      if coords[i]>coords[i+1]:
+        max_val= coords[i+1]
+    iters = {
+        n: count
+    }
+    scale=5
+    display = pygame.display.set_mode()
+    display.set_colorkey("white")
+    pygame.draw.lines(display, "black", False, coords*5)
+    new_display = pygame.transform.flip(display, False, True)
+    display.blit(new_display, (0, 0))
+    font = pygame.font.Font(None, 10)
+    msg = font.render("msg", False, "white")
+    display.blit(msg, (10, 10))
+
+
+foo(10)
