90d191ce7c0c0b61595cd84a7dfefeea2bc2d2af
Lillian Thorne <112438914+lthorne3@users.noreply.github.com>
2022-10-18 03:17:56 +0000

    boolean

---
+++ ch05/exercises/boolean_functions.py
@@ -0,0 +1,28 @@
+score= int(input("What is your score out of 100? "))
+
+def percentage_to_letter(score=0):
+  if score >= 90: return "A"
+  elif score >= 80: return "B"
+  elif score >= 70: return "C"
+  elif score >= 60: return "D"
+  else : return "F"
+
+letter= percentage_to_letter(score)
+
+def is_passing(letter=None):
+  if letter== "C":
+    return True
+  elif letter=="B":
+    return True
+  elif letter=="A":
+    return True
+  else:
+    return False
+
+is_passing(letter)
+
+if is_passing(letter)==True:
+  print("You are passing.")
+elif is_passing(letter)==False:
+  print("You are failing")
+  
