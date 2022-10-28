c808e14d8e9bcef534cdbf8c2a77a05434ec1695
Lillian Thorne <112438914+lthorne3@users.noreply.github.com>
2022-10-28 02:05:44 +0000

    midterm

---
+++ ch06/exam/README.md
@@ -0,0 +1,13 @@
+# CS110 Midterm Exam
+
+## SHORT DESCRIPTION 
+I used the turtle library and the random library to create the image of a night sky, with a crescent moon and 30 stars. I used 2 functions, one for drawing the circles that creates the moon and one for drawing stars. 
+
+## KNOWN BUGS AND INCOMPLETE PARTS 
+n/a
+
+## REFERENCES 
+doc.python.org (turtle library and random library)
+
+## MISCELLANEOUS COMMENTS 
+n/a
---
+++ ch06/exam/midterm.py
@@ -0,0 +1,47 @@
+import turtle
+import random
+
+#night sky- draw background and fill 
+#draw stars maybe fill?
+#moon- draw on filled moon color circle and one a little to the left of it that bg colored to make a crescent
+
+t=turtle.Turtle()
+window=turtle.Screen()
+window.screensize(100,100)
+cols= ["black", "white", "yellow"]
+window.bgcolor(cols[0])
+
+
+def draw_moon(radius, color):
+  t.begin_fill()
+  c=color
+  t.color(c)
+  r=radius
+  t.circle(r)
+  t.end_fill()
+  
+
+def draw_star(f):
+  f
+  t.color(cols[1])
+  for i in range(0,5):
+    t.forward(f)
+    t.right(144)
+
+
+
+def main():
+  t.back(30)
+  draw_moon(50, "yellow")
+  t.forward(15)
+  draw_moon(50,"black")
+  for i in range(0,30):
+    t.pu()
+    t.right(random.randint(10, 350))
+    t.forward(random.randint(30,60))
+    t.pd()
+    draw_star(random.randint(10,40))
+
+
+main()
+window.exitonclick()
