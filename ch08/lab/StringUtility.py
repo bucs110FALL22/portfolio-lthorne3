cec65676c4915af73cdafbcb28ec2860c6c7e306
Lillian Thorne <112438914+lthorne3@users.noreply.github.com>
2022-11-11 20:20:13 +0000

    string utility lab

--- ch08/lab/StringUtility.py
+++ ch08/lab/StringUtility.py
@@ -3,36 +3,36 @@ class StringUtility:
     def __init__(self, string):
         self.string = string
 
-    def ___str__(self):
+    def __str__(self):
+        return self.string
+        
+    def vowels(self):
+        count = 0
+        for i in range(0, len(self.string)):
+            if self.string[i] in "aeiouAEIOU":
+                count += 1
+          
+        return count
+
+    def bothEnds(self):
+        self.string = self.string[0] + self.string[1] + self.string[len(self.string-1)] + self.string[-1]
+        if len(self.string) <= 2:
+            self.string = " "
         return self.string
 
-    # def vowels(self):
-    #     count = 0
-    #     for i in range(0, len(self.string)):
-    #         if self.string[i] in "aeiouAEIOU":
-    #             count += 1
-    #     return count
+    def fixStart(self):
+        char = self.string[0]
+        self.string = self.string.replace(char, '*')
+        self.string = char + self.string[1:]
+        return self.string
 
-    # def bothends(self):
-    #     self.string = self.string[1] + self.string[2] + self.string[
-    #         -2] + self.string[-1]
-    #     if len(self.string) <= 2:
-    #         self.string = " "
-    #     return self.string
+    def asciiSum(self):
+        sum = None
+        for i in range(0, len(self.string)):
+            sum += ord.self.string[i]
+        return sum
 
-    # def fixstart(self):
-    #     char = self.string[0]
-    #     self.string = self.string.replace(char, '*')
-    #     self.string = char + self.string[1:]
-    #     return self.string
-
-    # def asciiSum(self):
-    #     sum = None
-    #     for i in range(0, len(self.string)):
-    #         sum += ord.self.string[i]
-    #     return sum
-
-    # def cipher(self):
-    #     for i in range(0, len(self.string)):
-    #         self.string[i] = chr(ord(self.string[0]) + len(self.string))
-    #     return self.string
+    def cipher(self):
+        for i in range(0, len(self.string)):
+            self.string[i] = chr(ord(self.string[0]) + len(self.string))
+        return self.string
