384162e806a52635389030701c51e6697f6cc19f
Lillian Thorne <112438914+lthorne3@users.noreply.github.com>
2022-12-10 01:16:42 +0000

    aetrsydxt

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,43 +1,49 @@
-#import pygame
-
-coords = []
-n=None
-count=None
-max_so_far=None
+import pygame
 
 
 
-def foo():
-  count = 0      
-  upper_limit= 10
-  iters= {}
+
+def foo():     
+  upper_limit= 20
+  iters= {
+    1:0
+  }
   start= 2
-  max_so_far=count
-  for n in range(start, upper_limit):
-    while n != 1:
-      if n % 2 == 0:
-        n = n // 2
+  max_so_far=0
+  for n in range(start, upper_limit+1):
+    i=n
+    count= 0
+    while i != 1:
+      if i % 2 == 0:
+        i = i // 2
         count += 1
       else:
-        n = n * 3 + 1
-        count += 1     
-      if count> max_so_far:
-        max_so_far=count
-      iters.update({n : count})
+        i = i * 3 + 1
+        count += 1 
+    if count > max_so_far:
+      max_so_far=count
+    iters.update({n : count})
+
+
   print(iters)
   print("Max num of iterations: ", max_so_far)
           
-  # scale=5
-  # pygamae.init()
-  # display = pygame.display.set_mode()
-  # display.set_colorkey("white")
-  # pygame.draw.lines(display, "black", False, {iters})
-  # new_display = pygame.transform.flip(display, False, True)
-  # display.blit(new_display, (0, 0))
-  # font_name= None
-  # font = pygame.font.init(font_name, 10)
-  # msg = font.render(f"Max # of iterations: {max_so_far}", False, "white")
-  # display.blit(msg, (10, 10))
+
+  scale=10
+  coords= list(iters.items())
+  pygame.init()
+  display = pygame.display.set_mode()
+  display.set_colorkey("white")
+  new_display = pygame.transform.flip(display, False, True)
+  display.blit(new_display, (0,0))
+  pygame.draw.lines(display, "red", False, coords*scale)
+  font_name= None
+  pygame.font.init()
+  font= pygame.font.Font(font_name, 30)
+  msg = font.render(f"Max # of iterations: {max_so_far}", False, "white")
+  display.blit(msg, (0,200))
+  pygame.display.flip()
+  pygame.display.exitonclick()
 
 
 foo()
